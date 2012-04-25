6accbc2561e5fb14e269eb491320a820ee1a17d4
Emilio López <turl@tuxfamily.org>
2012-04-25 00:58:20 -0300

    Add invalid input handling and drop some debug prints

--- cmstats/views/index.py
+++ cmstats/views/index.py
@@ -2,6 +2,7 @@ from cmstats.database.schema.devices import Device
 from cmstats.resources import Root
 from cmstats.utils.countries import population
 from pyramid.view import view_config
+from pyramid.httpexceptions import exception_response
 
 
 @view_config(context=Root, renderer='index.mako')
@@ -37,19 +38,24 @@ def perdevice_page(request):
     device = request.matchdict['device']
     country_data = []
 
+    kangs = Device.count_kang(device)
+    nonkangs = Device.count_nonkang(device)
+
+    #if there are no installs, this is not a valid device
+    if (kangs + nonkangs) == 0:
+        raise exception_response(400)
+
     for country_code, country_installs in Device.country_count(device):
         country = population.get(country_code, None)
         if country is not None:
             country_data.append((country[0], country_installs))
-    
-    print country_data
 
     kwargs = {
             'device': device,
             'country_data': country_data,
             'version_count': Device.version_count(device),
-            'total_nonkang': Device.count_nonkang(device),
-            'total_kang': Device.count_kang(device),
+            'total_nonkang': nonkangs,
+            'total_kang': kangs,
             'total_last_day': Device.count_last_day(device),
     }
 
@@ -60,9 +66,7 @@ def percountry_page(request):
     country_code = request.matchdict['country'].lower()
     country = population.get(country_code, None)
     if country is None:
-        return 'Invalid country'
-    
-    print country
+        raise exception_response(400)
     
     kwargs = {
             'country': country[0],
